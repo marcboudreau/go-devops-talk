7941c1895efe9354d3633e0e369283b63f3135ad
Marc Boudreau <mboudreau@accedian.com>
2019-01-29 14:01:29 -0500

    More refinements to Makefile and run-awesome.sh script

--- run-awesome.sh
+++ run-awesome.sh
@@ -5,5 +5,19 @@ server=$1
 private_key=${SSH_PRIVATE_KEY_FILE:-${HOME:-.}/.ssh/id_rsa}
 public_key=${SSH_PUBLIC_KEY_FILE:-${private_key}.pub}
 
+catapult/bin/catapult -k $private_key -p $public_key -l tcp:127.0.0.1:2375 -r unix:/var/run/docker.sock user@$server &
+catapult_pid=$!
 
-go run catapult/cmd/catapult/main.go -k $private_key 
+echo "Waiting for tunnel to be established..."
+
+while ! netstat -ant | grep LISTEN | grep 2375 >/dev/null; do
+  echo -n "."
+done
+echo ""
+
+export DOCKER_HOST=tcp://127.0.0.1:2375
+
+docker version
+docker ps -a
+
+kill $catapult_pid
